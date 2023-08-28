d0fb85df0beb4aa51e2cdcd32a71f27a5079f92a
Patrick Sinaga <sinagapatrick610@gmail.com>
2023-08-28 12:37:39 +0700

    Initial Commit

---
+++ SetaraV2/SetaraV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SetaraV2App.swift
+//  SetaraV2
+//
+//  Created by Patrick Samuel Owen Saritua Sinaga on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SetaraV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
